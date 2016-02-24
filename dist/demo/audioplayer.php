6a64925aff90de03afa39607e553ed3a1488f255
Ben <ben@coterie.co.ke>
2016-02-24 15:41:06 +0300

    ui

--- dist/demo/audioplayer.php
+++ dist/demo/audioplayer.php
@@ -3,8 +3,39 @@
 <head>
 <meta charset="utf-8">
 <title>Audio Player demo</title>
+<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="../css/audioplayer.min.css" rel="stylesheet" type="text/css">
+<style>
+body
+{
+	padding:100px 40px;
+}
+</style>
 </head>
 
 <body>
+
+<div class="audioplayer">
+	<div class="controls play-controls">
+    	<div class="control-holder">
+        	<div class="control-button">
+            	<i class="fa fa-play"></i>
+            </div>
+        </div><!-- .control-holder -->
+    </div><!-- .controls -->
+    <div class="seekbar">
+    	<div class="states-holder"></div><!-- .states-holder -->
+        <div class="indicator"></div><!-- .indicator -->
+    </div><!-- .seekbar -->
+    <div class="volume-control controls">
+    	<div class="volume-toggle">
+        	<div class="control-button">
+            	<i class="fa fa-volume-up"></i>
+            </div><!-- .volume-icon -->
+        </div><!-- .controls -->
+        <div class="volume-popup"></div><!-- .volume-popup -->
+    </div><!-- .controls -->
+</div><!-- .audioplayer-->
 </body>
 </html>
