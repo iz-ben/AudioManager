41a5965cd8642cf402b7a51956e8837f53d6a43f
Ben <ben@coterie.co.ke>
2016-02-24 15:40:29 +0300

    install less & cssmin

---
+++ grunt/less.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+module.exports = {
+	compileResponsiveTheme:
+	{
+		options: {
+          paths: []
+        },
+		src: 'src/less/audioplayer.less',
+		dest: 'dist/css/audioplayer.css'
+	}
+};
