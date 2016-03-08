a3a2da6fdb558cae80c390ea2c1bb092a7d20638
Ben <ben@coterie.co.ke>
2016-03-08 14:14:39 +0300

    update playlist and added soundcloud example track

--- dist/demo/audiomanager.php
+++ dist/demo/audiomanager.php
@@ -18,18 +18,28 @@
 <ul>
 	<li><a href="#" onClick="audiomanager.playSound(blue); return false;">Eiffel65 - Blue</a></li>
 	<li><a href="#" onClick="audiomanager.playSound(roma); return false;">Dean Martin - On an evening in Roma</a></li>
+    <li><a href="#" onClick="audiomanager.playSound(violin); return false;">Amira Saleh2 - when the violin talks (Soundcloud)</a></li>
 </ul>
 
 
 <script src="../js/audiomanager.min.js"></script>
 <script>
-var audiomanager = AudioManager,
+var audiomanager = AudioManager;
 
-blue = audiomanager.createSound('http://localhost/audiomanager/dist/mp3/eiffel65-blue.mp3','Eiffel 65 - Blue'),
+audiomanager.setSCKey('29e520b21e2ce8431d8a2b0f88e84a8b');
 
-roma = audiomanager.createSound('http://localhost/audiomanager/dist/mp3/dean-martin-on-an-evening-in-roma.mp3','Dean Martin - On an Evening in Roma');
+var blue = audiomanager.createSound('http://localhost/audiomanager/dist/mp3/eiffel65-blue.mp3','Eiffel 65 - Blue')
+
+var roma = audiomanager.createSound('http://localhost/audiomanager/dist/mp3/dean-martin-on-an-evening-in-roma.mp3','Dean Martin - On an Evening in Roma');
+
+var violin = audiomanager.createSound('https://soundcloud.com/amirra-salleh/when-the-violin-speaks','Amira Saleh2 - when the violin talks');
 
 //audiomanager.play();
+
+audiomanager.byteFrequencyData = function(data)
+{
+	//console.log(data);
+}
 
 </script>
 
